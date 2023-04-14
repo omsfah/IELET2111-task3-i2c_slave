4866aec593f5e1e02412d4c4c01e5455e11c1377
raflemakt <raflemakt@hotmail.com>
2023-04-14 12:58:09 +0200

    Add global configuration file

---
+++ src/configuration.h
@@ -0,0 +1,13 @@
+/*
+ * configuration.h
+ *
+ * Global configuration file for the
+ * I2C target fan/voltage monitoring system
+ *
+ * board: AVR128DB48 Curiosity Nano
+ *
+ */
+
+#define F_CPU 4000000UL
+
+#define I2C_TARGET_DEFAULT_ADDRESS 4
