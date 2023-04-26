c02d556644daadf7d729181eec1c9b9aea4ebc34
raflemakt <raflemakt@hotmail.com>
2023-04-26 22:34:45 +0200

    Remove old tests from esp32 I2C controller sketch

--- tools/esp32_i2c_master_usart_bridge/src/main.cpp
+++ tools/esp32_i2c_master_usart_bridge/src/main.cpp
@@ -19,117 +19,52 @@ static const uint8_t DEBUG_INCREMENTER_MIN = 48;
 static const uint8_t DEBUG_INCREMENTER_MAX = 57;
 
 uint8_t number = DEBUG_INCREMENTER_MIN;
-uint8_t data_buffer[PACKET_SIZE] = {};
+uint8_t data_buffer[PACKET_SIZE] = {0};
 
 
-void print_buffer(uint8_t bytes) {
-    // Print the received data over USART
+void printBuffer(uint8_t bytes) {
+    /* Print the received data over USART */
     for (auto i = 0; i < bytes; i++) {
-        Serial.println(data_buffer[i]);
+        Serial.printf("%02X", data_buffer[i]);
+        if ((i+1) % 4 == 0) Serial.println();
     }
     Serial.println();
 }
 
-void request_data_from_target(uint8_t bytes) {
-    Serial.printf("requesting data from target\n");
-    Wire.beginTransmission(TARGET_I2C_ADDRESS);
-
-    // End transmission and record potential error code
-    uint8_t i2c_send_result = Wire.endTransmission(true);
-    Serial.printf("endTransmission: %u\n", i2c_send_result);
-
-    uint8_t bytes_received = Wire.requestFrom(TARGET_I2C_ADDRESS, bytes);
-    Serial.printf("reading %u bytes from target\n", bytes_received);
-    Wire.readBytes(data_buffer, bytes_received);    // Move data into buffer
-
-    // End transmission and record potential error code
-    i2c_send_result = Wire.endTransmission(true);
-    Serial.printf("endTransmission: %u\n", i2c_send_result);
-    
-    print_buffer(bytes_received);
-}
-
-void old(void) {
-    // Establish communication of address packet
-    Wire.beginTransmission(TARGET_I2C_ADDRESS);
-
-    // Optionally send something to the slave
-    number++;
-    if (number > DEBUG_INCREMENTER_MAX) {
-        number = DEBUG_INCREMENTER_MIN;
-    }
-    Serial.printf("Sending data: %d\n", number);
-    Wire.write(number);
-    Wire.write(0);
-    Wire.write(0);
-    Wire.write(255);
-    Wire.write(0);
-    Wire.write(255);
-
-    // End transmission and record potential error code
-    uint8_t i2c_send_result = Wire.endTransmission(true);
-    Serial.printf("endTransmission: %u\n", i2c_send_result);
-
-    // Read bytes from slave
-    uint8_t bytes_received = Wire.requestFrom(TARGET_I2C_ADDRESS, PACKET_SIZE);
-    Serial.printf("requestFrom: %u\n", bytes_received);
-
-    // If we receive more than 0 bytes, send over USART
-    if (bytes_received > 0) {
-        uint8_t _receive_buffer[bytes_received];            // Allocate buffer space 
-        Wire.readBytes(_receive_buffer, bytes_received);    // Move data into buffer
-        for (auto i = 0; i < bytes_received; i++) {
-            Serial.println(_receive_buffer[i]);
-        }
-    }
-    Serial.printf("\n");
-}
 
 void sendCommand(uint8_t command_num, uint32_t value) {
+    Serial.printf("Command: %d value: %d\n", command_num, value);
     Wire.beginTransmission(TARGET_I2C_ADDRESS);
 
-    Serial.printf("Command: %d value: %d\n", command_num, value);
-
+    // Serialize the u32 value to four unsigned bytes 
     uint8_t _v4 = (uint8_t) (value);
     uint8_t _v3 = (uint8_t) (value >> 8);
     uint8_t _v2 = (uint8_t) (value >> 16);
     uint8_t _v1 = (uint8_t) (value >> 24);
 
-    Wire.write(command_num);
-    Wire.write(_v1);
+    // 5 byte command packet, as used by 'I2C_parseCommand'
+    Wire.write(command_num);    // Command (see 'I2C_COMMAND' in 'datastructure.h)
+    Wire.write(_v1);    // MSB
     Wire.write(_v2);
     Wire.write(_v3);
-    Wire.write(_v4);
+    Wire.write(_v4);    // LSB
 
     // End transmission and record potential error code
     uint8_t i2c_send_result = Wire.endTransmission(true);
     Serial.printf("endTransmission: %u\n", i2c_send_result);
+
     // Read bytes from slave
     uint8_t bytes_received = Wire.requestFrom(TARGET_I2C_ADDRESS, PACKET_SIZE);
     Serial.printf("requestFrom: %u\n", bytes_received);
-}
+    Wire.readBytes(data_buffer, bytes_received);    // Move data into buffer
 
-void send_incrementing_number(void) {
-    // Establish communication of address packet
-    Wire.beginTransmission(TARGET_I2C_ADDRESS);
-
-    number++;
-    if (number > DEBUG_INCREMENTER_MAX) {
-        number = DEBUG_INCREMENTER_MIN;
-    }
-    Serial.printf("Sending data: %d\n", number);
-    Wire.write(number);
-
-    // End transmission and record potential error code
-    uint8_t i2c_send_result = Wire.endTransmission(true);
-    Serial.printf("endTransmission: %u\n", i2c_send_result);
-
+    printBuffer(bytes_received);
 }
 
 void setup(void) {
     Serial.begin(115200);
     Serial.setDebugOutput(true);
-    Wire.end();
+    //Wire.end();
     Wire.begin();   // Default SDA 21, SCL 22
 
     Serial.println("hello there");
@@ -139,9 +74,6 @@ void loop(void) {
     // In the loop, ESP32 polls the slave for data every POLLING_DELAY_ms
     delay(POLLING_DELAY_ms);
 
-    request_data_from_target(53);
-    //old();
-    //sendCommand(3, 31337);
-    //send_incrementing_number();
+    sendCommand(0x64, 0x1ff);
     Serial.println();
 }
